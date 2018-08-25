d0b7d1c26e52307706c20abc050a7a267311f58e
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-25 12:48:10 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ from flask import Flask,requests,make_response
 import os,json
 import pyowm
 
-app = Flask(__name__)
+'''app = Flask(__name__)
 owmapikey=os.environ.get('4880f488e7141442da2967d2d90954f6') #or provide your key here
 owm = pyowm.OWM(owmapikey)
 
@@ -58,7 +58,89 @@ def processRequest(req):
         "source": "dialogflow-weather-by-satheshrgs"
         }
     except Exception as e:
-                         print(e)
+                         print(e)'''
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    if req.get("result").get("action") == "yahooWeatherForecast":
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeYqlQuery(req)
+        if yql_query is None:
+            return {}
+        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+        result = urlopen(yql_url).read()
+        data = json.loads(result)
+        res = makeWebhookResult(data)
+    else:
+        print("action is not yahooweatherforecast")
+        return {}
+    return res
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
 
 if __name__ == '__main__':
      port = int(os.getenv('PORT', 5000))
