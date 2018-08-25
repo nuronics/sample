1fbfbf115d81e659834908941c28ae6d4ee88b70
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-25 14:53:07 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -22,17 +22,15 @@ def webhook():
 
 def processRequest(req):
     
-   ' if req.get("result").get("action") == "yahooWeatherForecast":
-        print("req has got some thing")
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
-        yql_query = makeYqlQuery(req)
-        if yql_query is None:
-            return {}
-        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-        result = urlopen(yql_url).read()
-        data = json.loads(result)
-        res = makeWebhookResult(data)
-        return res
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+          return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
 
 def makeYqlQuery(req):
     result = req.get("result")
