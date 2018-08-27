22e6e62f1538536c2024835bf3452b1f84ad67b8
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-27 12:51:03 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+    req = Request.get_json(silent=True, force=True)
 
     res = processRequest(req)
 
