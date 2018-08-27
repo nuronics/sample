48312bf3fc007979aa84f12403da5aae6ac4cbb5
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-27 15:43:50 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -85,4 +85,4 @@ def makeWebhookResult(data):
 if __name__ == '__main__':
      port = int(os.getenv('PORT', 5000))
      print("Starting app on port %d" % port)
-     app.run(debug=True,port=port)
+     app.run(debug=True,port=port,host=0.0.0.0)
