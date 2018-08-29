2dcb679c75fb8f861f807bfe74550068b2e525ac
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-29 17:59:19 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -36,23 +36,6 @@ def processRequest(req):
     json_data = requests.get(url).json()
     weatherDesc = getweatherDesc(json_data)  
     
-   ''' w = observation.get_weather()
-    latlon_res = observation.get_location()
-    lat = str(latlon_res.get_lat())
-    lon = str(latlon_res.get_lon())
-
-    wind_res = w.get_wind()
-    wind_speed = str(wind_res.get('speed'))
-
-    humidity = str(w.get_humidity())
-
-    celsius_result = w.get_temperature('celsius')
-    temp_min_celsius = str(celsius_result.get('temp_min'))
-    temp_max_celsius = str(celsius_result.get('temp_max'))
-
-    fahrenheit_result = w.get_temperature('fahrenheit')
-    temp_min_fahrenheit = str(fahrenheit_result.get('temp_min'))
-    temp_max_fahrenheit = str(fahrenheit_result.get('temp_max'))'''
     speech = "Today the weather in " + city + ": \n" +weatherDesc
     print(speech)
     return {
